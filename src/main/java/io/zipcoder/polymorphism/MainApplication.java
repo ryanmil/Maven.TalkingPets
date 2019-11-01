b057eb89ee81acf00b86b7df82730839f7d57c82
Ryan <ryan@rmiller.dev>
2019-11-01 19:27:40 -0400

    make pet classes

--- src/main/java/io/zipcoder/polymorphism/MainApplication.java
+++ src/main/java/io/zipcoder/polymorphism/MainApplication.java
@@ -1,7 +1,30 @@
 package io.zipcoder.polymorphism;
 
+import java.util.Scanner;
+
 /**
  * Created by leon on 11/6/17.
  */
 public class MainApplication {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("How many pets do you have");
+        int numberOfPets = Integer.parseInt(scanner.nextLine());
+
+        String[] petTypes = new String[numberOfPets];
+        for (int i = 0; i < petTypes.length; i++) {
+            System.out.println("What type of pet is pet #" + (i + 1));
+            petTypes[i] = scanner.nextLine();
+
+            System.out.println("What is this pet's name?");
+            String petName = scanner.nextLine();
+        }
+
+
+
+
+    }
 }
---
+++ src/main/java/io/zipcoder/polymorphism/Pet.java
@@ -0,0 +1,17 @@
+package io.zipcoder.polymorphism;
+
+public abstract class Pet {
+
+    String name;
+
+    public Pet(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract String speak();
+
+}
