49c94f4bb0d11a4fe34caef984ac7dd012590d28
kendrang <kendrang@users.noreply.github.com>
2019-11-02 14:08:32 -0400

    ryan messed up haha

--- src/test/java/io/zipcoder/polymorphism/MainApplicationTest.java
+++ src/test/java/io/zipcoder/polymorphism/MainApplicationTest.java
@@ -1,7 +1,20 @@
 package io.zipcoder.polymorphism;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by leon on 11/6/17.
  */
 public class MainApplicationTest {
+    @Test
+    public void dogTest() {
+        Dog dog = new Dog("Woofie");
+        String actual =  dog.speak();
+        String expected = "Woof";
+        Assert.assertEquals(expected,actual);
+
+
+    }
+
 }
